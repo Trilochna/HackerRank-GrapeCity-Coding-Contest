d872e68280d8068f287d213bea3f81b92205f757
TRILOCHNA <vaidtrilochna@gmail.com>
2023-09-25 22:58:33 +0530

    Update Print last 'K' elements.java

--- Print last 'K' elements/Print last 'K' elements.java
+++ Print last 'K' elements/Print last 'K' elements.java
@@ -1,1 +1,21 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner scn = new Scanner(System.in);
+        int N = scn.nextInt();
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = scn.nextInt();
+        }
+        int K = scn.nextInt();
+        
+        for(int j = arr.length - K; j < N; j++){
+            System.out.print(arr[j] + " ");
+        }
+    }
+}
