795fac511002664e5412e17febddfb21d64f2e9b
TRILOCHNA <vaidtrilochna@gmail.com>
2023-09-25 15:44:49 +0530

    Create Counting Vowels.java

---
+++ Counting Vowels/Counting Vowels.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        int count = 0;
+        
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+                count++;
+            } 
+        }
+        System.out.println(count);
+        
+    }
+}
