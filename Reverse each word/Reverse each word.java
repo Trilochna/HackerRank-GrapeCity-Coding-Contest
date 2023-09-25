08e883d0dd9e3ed60d09b29944f56299f87bdb83
TRILOCHNA <vaidtrilochna@gmail.com>
2023-09-25 16:24:25 +0530

    Update Reverse each word.java

--- Reverse each word/Reverse each word.java
+++ Reverse each word/Reverse each word.java
@@ -1,1 +1,26 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner scn = new Scanner(System.in);
+        String sentence = scn.nextLine();
+        // we will use a delimiter to parttion the string between separate words
+        String[] arr = sentence.split(" "); // the array will store separate words in the sentence string
+        String ans = ""; // default string to be returned/added to the sentence for output as a sentence having each word reversed
+        
+        for(int i = 0; i < arr.length; i++){
+            String temp = arr[i];
+            String defaultString = "";
+            
+            for(int j = temp.length() - 1; j >= 0; j--){
+                defaultString += temp.charAt(j);
+            }
+            ans += defaultString + " ";
+        }
+        System.out.println(ans);
+    }
+}
